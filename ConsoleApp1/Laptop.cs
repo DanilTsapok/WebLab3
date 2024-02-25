d4cb6b569cdd066a6708e5ebbc1beaa941b14f82
DanilTsapok <daniltsapok12012004@gmail.com>
2024-02-25 16:35:59 +0300

    lab4

---
+++ ConsoleApp1/Laptop.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Laptop
+    {
+        private int id;
+        private string brand;
+        public int screenSize;
+        protected double price;
+        internal bool isNew;
+        public string operatingSystem;
+        public Laptop(int id, string brand, int screenSize, double price, bool isNew, string operatingSystem)
+        {
+            this.id = id;
+            this.brand = brand;
+            this.screenSize = screenSize;
+            this.price = price;
+            this.isNew = isNew;
+            this.operatingSystem = operatingSystem;
+
+        }
+        public void getLaptopPrice()
+        {
+            Console.WriteLine($"Price is {price}");
+        }
+        protected void getScreenSize()
+        {
+            Console.WriteLine($"The screen size of the laptop is {screenSize}");
+        }
+        public void getBrand(Laptop laptop) => Console.WriteLine($"Laptop Brand:{laptop.brand}");
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,58 +1,42 @@
-﻿namespace ConsoleApp1
+﻿using System.Reflection;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            ThreadTest();
-            AsyncMethod();
-            GetInfoFromApi();
-        }
-        static void ThreadTest()
-        {
-
-            Thread t1 = new Thread(new ThreadStart(Method1));
-            Thread t2 = new Thread(new ThreadStart(Method2));
-            t1.Start();
-            t2.Start();
+            Laptop laptopLenovo = new(1, "Lenovo", 14, 15.300, true, "Windows");
+            Laptop laptopMacbook = new(2, "Macbook", 14, 45.300, false, "MacOS");
+            Type laptopType = typeof(Laptop);
+            TypeInfo typelaptopInfo = laptopType.GetTypeInfo();
+            Console.WriteLine($"TypeName:{typelaptopInfo.Name}");
 
 
-            static void Method1()
+            MemberInfo[] members = laptopType.GetMembers();
+            Console.WriteLine("Members:");
+            foreach (MemberInfo member in typelaptopInfo.DeclaredMembers)
             {
-                Console.WriteLine("Method 1 is executing");
-                Thread.Sleep(2000);
-                Console.WriteLine("Method 1 finished");
-
+                Console.WriteLine(member.Name );
             }
-            static void Method2()
+            Console.WriteLine("Fields:");
+            foreach(FieldInfo fileInfo in typelaptopInfo.DeclaredFields) {
+                Console.WriteLine(fileInfo.Name);
+            }
+            Console.WriteLine("MethodInfo:");
+            foreach (MethodInfo methodInfo in typelaptopInfo.DeclaredMethods)
             {
-                Console.WriteLine("Method 2 is executing");
-                Thread.Sleep(3000);
-                Console.WriteLine("Method 2 finished");
-
+                Console.WriteLine(methodInfo.Name);
             }
-        }
-        static async Task AsyncMethod()
-        {
-            Console.WriteLine("start");
-            await Task.Delay(3000);
-            Console.WriteLine("finish");
+
+            Console.WriteLine("Reflection:");
+            MethodInfo methodInfo1 = laptopType.GetMethod("getBrand");
+            if(methodInfo1 != null)
+            {
+                methodInfo1.Invoke(laptopLenovo, [laptopMacbook]);
+            }
+
         }
 
-        static async Task GetInfoFromApi()
-        {
-            Console.WriteLine("GetInfoFromApi");
-            var client= new HttpClient();
-            var response = await client.GetAsync("https://jsonplaceholder.org/comments/1");
-            if (response != null)
-            {
-                string data =  await response.Content.ReadAsStringAsync();
-                Console.WriteLine($"InfoFromApi:{data}");
-            }
-            else
-            {
-                Console.WriteLine("Error");
-            }
-        }
     }
 }
